6b62a1368f91195b2f5451652e5f67147de0fb6f
Ivan Pedrazas <ipedrazas@AirBug.local>
2019-09-25 13:53:24 +0100

    added secrets

--- goto/handler.go
+++ goto/handler.go
@@ -1,9 +1,13 @@
 package function
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
 func Handle(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "https://google.com/", http.StatusTemporaryRedirect)
+	// http.Redirect(w, r, "https://google.com/", http.StatusTemporaryRedirect)
+	apiKey, _ := ioutil.ReadFile("/var/openfaas/secrets/api-key")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(apiKey))
 }
